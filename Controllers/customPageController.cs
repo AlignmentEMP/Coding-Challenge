5979c85e68309b330b98b09399d69d6de6ac7f6c
AlignmentEMP <56951382+AlignmentEMP@users.noreply.github.com>
2021-06-01 16:08:07 -0600

    Initial commit

---
+++ Controllers/customPageController.cs
@@ -0,0 +1,19 @@
+﻿using EPiServer;
+using EPiServer.Core;
+using EPiServer.Framework.DataAnnotations;
+using EPiServer.Web.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using testSite.Models.Pages;
+
+namespace testSite.Controllers
+{
+    public class customPageController : PageControllerBase<customPage>
+    {
+        public ActionResult Index(customPage currentPage)
+        {
+            return View(currentPage);
+        }
+    }
+}
---
+++ Models/Pages/customPage.cs
@@ -0,0 +1,25 @@
+﻿using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace testSite.Models.Pages
+{
+    [SiteContentType(GUID = "0a93e7da-a6f2-4116-a986-29e988467e08")]
+    [SiteImageUrl(Global.StaticGraphicsFolderPath + "page-type-thumbnail-custom.png")]
+    public class customPage : SitePageData
+    {
+        [Display(
+            GroupName = SystemTabNames.Content,
+            Order = 310)]
+        [CultureSpecific]
+        public virtual XhtmlString MainBody { get; set; }
+
+        [Display(
+            GroupName = SystemTabNames.Content,
+            Order = 320)]
+        public virtual ContentArea MainContentArea { get; set; }
+    }
+}
